7d37dc577afcfb0c56a238bf0ead5516190a798a
Pradeep Godithi <godithipradeep@gmail.com>
2022-02-13 16:45:23 +0530

    Update ansible.sh

--- terraform/ansible.sh
+++ terraform/ansible.sh
@@ -1,10 +1,5 @@
 #! /bin/bash
 sudo apt update -y
-sudo apt install -y software-properties-common
-sudo add-apt-repository --yes --update ppa:ansible/ansible
-sudo apt install ansible -y
-sudo apt install git -y
-sudo apt update
 sudo apt install -y openjdk-8-jdk openjdk-8-jre
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
@@ -12,3 +7,8 @@ sudo apt update -y
 sudo apt install jenkins -y
 sudo systemctl start jenkins
 sudo systemctl enable jenkins
+sudo apt update -y
+sudo apt install -y software-properties-common
+sudo add-apt-repository --yes --update ppa:ansible/ansible
+sudo apt install ansible -y
+sudo apt install git -y
